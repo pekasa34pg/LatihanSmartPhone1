32fed9a188bef778560aa39075af6b7c31aa8f6b
perkasa tricahya putra dewa patrikha <perkaspatrikha123@gmail.com>
2026-04-17 10:27:24 +0700

    PenyelesaiyanISmartphone

--- LatihanSmartPhone1/Program.cs
+++ LatihanSmartPhone1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ISmartphone hpBudi = new IPhone();
+
+Fotografer fotografer = new Fotografer();
+fotografer.Kerja(hpBudi);
+
+hpBudi = new Samsung();
+fotografer.Kerja(hpBudi);
+
+interface ISmartphone
+{
+    public void Ambilfoto();
+}
+
+class IPhone : ISmartphone
+{
+    public void Ambilfoto()
+    {
+        Console.WriteLine("Mengaktifkan sensor kamera...Cekrek");
+    }
+}
+
+class Samsung : ISmartphone
+{
+    public void Ambilfoto()
+    {
+        Console.WriteLine("Mengaktifkan fitur beauty");
+    }
+}
+
+class Fotografer
+{
+    public void Kerja(ISmartphone hp)
+    {
+        hp.Ambilfoto();
+    }
+}
